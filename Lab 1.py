ba343a4daf8ae5f36ea921092acae7be08c9fc43
Emily Nixon <123026549+enixon01@users.noreply.github.com>
2023-01-25 15:57:10 -0500

    Add files via upload

---
+++ Lab 1.py
@@ -0,0 +1,42 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#The authors' names are: Emily Nixon, Vincenza Paradiso
+#Emily started as the driver
+print ("Hello, world")
+Hello, world
+print ("Welcome to CPSC 207")
+Welcome to CPSC 207
+print ("Hello /nWorld")
+Hello /nWorld
+print ("Hello \nWorld")
+Hello 
+World
+print ("How \nare \you?")
+How 
+are \you?
+print ("How \nare \nyou?")
+How 
+are 
+you?
+print ("Today" , "is" , "Wednesday")
+Today is Wednesday
+print ("Today" + "is" + "Wednesday")
+TodayisWednesday
+print ("Hello \n" + "\nWorld!")
+Hello 
+
+World!
+print ("123 \n45 \n6")
+123 
+45 
+6
+print ("2=2" , 2+2)
+2=2 4
+print ("2+2" ,2+2)
+2+2 4
+print ("2+2=",2+2)
+2+2= 4
+print("50 pounds is " , 50*0.453592, "Kilograms")
+50 pounds is  22.6796 Kilograms
+print("50 pounds is" , 50*0.453592, "Kilograms")
+50 pounds is 22.6796 Kilograms
